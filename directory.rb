f8902d6df56799cad4beddaaa092347380c2da05
Cong Cong Bo <cong@hippocampus.ai>
2017-06-23 20:47:46 +0100

    simplified code1

--- directory.rb
+++ directory.rb
@@ -12,8 +12,20 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each {|x| puts x}
 
-print "Overall, we have #{student.count} great students"
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each {|x| puts x}
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
