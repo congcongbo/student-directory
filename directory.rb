51ee80dbfa8c936a8580231cde8df9137419b1be
Cong Cong Bo <cong@hippocampus.ai>
2017-06-25 22:00:31 +0100

    saving

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ def print_menu
   puts "1. Input students"
   puts "2. List students"
   puts "3. Save the list to students.csv"
+  puts "4. Load student list from students.csv"
   puts "9. Exit"
 end
 
@@ -52,6 +53,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -74,6 +77,15 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+      @students << {name: name, cohort: cohort.to_sym}
+  end
+    file.close
 end
 
 interactive_menu
