a5430fb3becfd8dd15173459be47fbf90ebe487c
Cong Cong Bo <cong@hippocampus.ai>
2017-06-25 21:48:27 +0100

    refactored more

--- directory.rb
+++ directory.rb
@@ -34,6 +34,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. List students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -49,6 +50,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -61,6 +64,16 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
