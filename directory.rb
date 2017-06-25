5c4a322cfe36be38d39e621f8c31e8741cd1a12a
Cong Cong Bo <cong@hippocampus.ai>
2017-06-25 20:57:40 +0100

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,12 +1,15 @@
 def input_students
-    puts "Please enter names of students"
-    puts "Hit return twice to finish"
     students = []
+    puts "Please enter names and cohorts of students"
+    puts "Hit return twice to finish"
     name = gets.chomp
+    puts "Which cohort?"
+    cohort = gets.chomp
     while !name.empty? do
-      students << {name: name, cohort: :november}
+      students << {name: name, cohort: cohort}
       puts "Now we have #{students.count} students"
       name = gets.chomp
+      cohort = gets.chomp
     end
     students
 end
@@ -16,15 +19,39 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each { |x| puts "#{x[:name]} (#{x[:cohort]} cohort)" }
+def print_students(names)
+  if names.length <0
+    names.each_with_index { |x,i| puts "#{i+1}. #{x[:name]} (#{x[:cohort]} cohort)" }
+  else
+    puts "We have no students yet"
+  end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} great students" unless names.length == 0
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input students"
+    puts "2. List students"
+    puts "9. Exit"
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
